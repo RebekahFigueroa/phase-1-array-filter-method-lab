cf9f7bce4ff5d52e0fdb03806bd80e2b6243c45d
Rebekah <figueroarebekah@gmail.com>
2022-12-22 23:26:32 -0600

    commit tree 895cd03f522b9445087e8e20fce6982addbe9acc

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+  return drivers.filter(function isMatch(driver) {
+    if (driver.toLowerCase() === name.toLowerCase()) {
+      return true;
+    }
+  });
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter((driver) => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function matchingName(driver) {
+    if (name === driver.name) {
+      return true;
+    }
+  });
+}
